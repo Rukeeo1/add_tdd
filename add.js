0aa5e909db09484e8cf86990a3f9a05d22978231
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-04-08 15:57:37 +0100

    upgraded my function...to allow addition of only numbers

--- add.js
+++ add.js
@@ -1,5 +1,9 @@
 const myAddObj = {
     add:function(a,b){
+        //lessons learnt. to check for if both are numbers...use 'or' and not &&.
+        if(typeof a !== 'number' || typeof b !== 'number'){
+            return 'first and second parameter\'s must be numbers'
+        }
         return a + b;
     }
 };
--- add.test.js
+++ add.test.js
@@ -2,8 +2,17 @@ const addObj = require('./add');
 
 //adding my first test...
 describe('Add Two Numbers', function(){
+    //my function has to return something...
     it('should return a value', function(){
         expect(addObj.add(2,2)).toBeDefined()
     });
+
+    it('should have a numbers a it\'s parameters', function(){
+        expect(addObj.add('2',2)).toBe('first and second parameter\'s must be numbers')
+    });
+    
 });
+
+
+
 
